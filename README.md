b1976c8e6fb0ade7337adff200f37882edf3391c
Sean Lee <46933582+ItsMeeSeanLee337@users.noreply.github.com>
2024-05-05 20:14:12 -0400

    V 2.13
    Fixed Typos

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### My personal Portfolio
-# V2.11
+# V2.13
 My personal portfolio website made using HTML, CSS, and JavaScript. The portfolio was also made using React, Tailwind, and Three.js.
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,10 +23,10 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-white text-[17px] max-w-3 leading-[30px]'
       >
-        I'm an aspiring software engineerer with experience front-end, back-end, and mobile 
+        I'm an aspiring software engineer with experience front-end, back-end, and mobile 
         development. Alongside development work I also have experience in data science and 
         maintaining IT systems. I've always had a passion for coding and seeing what's on 
-        the cutting edge of technology, which is exactly why I'm majoring in computer science 
+        the cutting edge of technology, which is exactly why I majored in computer science 
         and looking to expand my skill set. Outside of tech I'm interested in building guitars 
         and martial arts (Judo and Kendo). Keep scrolling to view my experience and what projects 
         I've worked on. Below are some of the languages and technologies that I'm familiar with.
